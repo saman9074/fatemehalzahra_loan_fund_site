9fba7424ebec769dede6b57e794091699d4891f6
ali abdi <abdi9074@gmail.com>
2024-04-17 01:11:22 +0330

    add field to users table

---
+++ database/migrations/2024_04_13_184956_add_field_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('family')->nullable()->after('name');
+            $table->string('national_code')->nullable()->after('family');
+            $table->string('post_code')->nullable()->after('national_code');
+            $table->text('home_address')->nullable()->after('post_code');
+            $table->string('jobs')->nullable()->after('home_address');
+            $table->text('work_address')->nullable()->after('jobs');
+            $table->string('username')->unique()->after('email');
+            $table->string('phone')->unique()->after('username');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
